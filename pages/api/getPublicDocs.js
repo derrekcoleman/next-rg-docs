cadf3db79e78819de08abb1eff5db67ef512ebc3
mackbowes <mackenziepbowes@gmail.com>
2021-10-23 17:12:35 -0700

    bugtesting api not working on vercel :)

--- pages/api/getPublicDocs.js
+++ pages/api/getPublicDocs.js
@@ -1,6 +1,7 @@
 import { getPublicDocs } from '../../lib/posts';
 
 export default function handler(req, res) {
+	console.log("Hello I'm Public Doc");
   const data = getPublicDocs();
   res.status(200).json({ data: data });
 }
